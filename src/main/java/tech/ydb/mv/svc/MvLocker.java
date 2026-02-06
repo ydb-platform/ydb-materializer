dad64a6cc67566ad36c243a0e38cfd45ee63dcc8
Max Zinal <zinal@ydb.tech>
2026-02-06 17:58:43 +0300

    each kesus lock should have its own session

--- src/main/java/tech/ydb/mv/svc/MvLocker.java
+++ src/main/java/tech/ydb/mv/svc/MvLocker.java
@@ -2,11 +2,13 @@ package tech.ydb.mv.svc;
 
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 import tech.ydb.coordination.CoordinationClient;
 import tech.ydb.coordination.CoordinationSession;
 import tech.ydb.coordination.SemaphoreLease;
 import tech.ydb.core.Result;
+import tech.ydb.query.tools.SessionRetryContext;
 
 import tech.ydb.mv.MvConfig;
 import tech.ydb.mv.YdbConnector;
@@ -19,17 +21,17 @@ public class MvLocker implements AutoCloseable {
 
     private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(MvLocker.class);
 
+    private final SessionRetryContext retryCtx;
     private final CoordinationClient client;
-    private final CoordinationSession session;
     private final String nodePath;
-    private final HashMap<String, SemaphoreLease> leases = new HashMap<>();
+    private final HashMap<String, Lease> leases = new HashMap<>();
     private final Duration timeout;
 
     public MvLocker(YdbConnector conn) {
+        this.retryCtx = conn.getQueryRetryCtx();
         this.client = conn.getCoordinationClient();
         this.nodePath = conn.getProperty(MvConfig.CONF_COORD_PATH, MvConfig.DEF_COORD_PATH);
         prepareNode(conn, this.nodePath);
-        this.session = obtainSession(conn, this.nodePath);
         int seconds = conn.getProperty(MvConfig.CONF_COORD_TIMEOUT, 10);
         if (seconds < 5) {
             seconds = 5;
@@ -44,11 +46,6 @@ public class MvLocker implements AutoCloseable {
         LOG.debug("Shutting down locking guard {} on `{}`",
                 System.identityHashCode(this), this.nodePath);
         releaseAll();
-        try {
-            session.close();
-        } catch (Exception ex) {
-            LOG.warn("Failed to close the coordination session", ex);
-        }
     }
 
     private static void prepareNode(YdbConnector conn, String nodePath) {
@@ -59,23 +56,8 @@ public class MvLocker implements AutoCloseable {
         ).join().expectSuccess();
     }
 
-    private static CoordinationSession obtainSession(YdbConnector conn, String nodePath) {
-        return conn.getQueryRetryCtx().supplyResult(
-                qs -> {
-                    var session = conn.getCoordinationClient().createSession(nodePath);
-                    return session.connect().thenApplyAsync(
-                            status -> status.isSuccess() ? Result.success(session) : Result.fail(status)
-                    );
-                }
-        ).join().getValue();
-    }
-
     public CoordinationClient getClient() {
         return client;
-    }
-
-    public CoordinationSession getSession() {
-        return session;
     }
 
     public String getNodePath() {
@@ -109,7 +91,7 @@ public class MvLocker implements AutoCloseable {
         name = YdbConnector.safe(name);
         LOG.debug("Ensuring the single `{}` job instance "
                 + "through lock with timeout {}...", name, timeout);
-        SemaphoreLease lease;
+        Lease lease;
         synchronized (leases) {
             lease = leases.get(name);
         }
@@ -118,14 +100,13 @@ public class MvLocker implements AutoCloseable {
             return true;
         }
         try {
-            lease = session.acquireEphemeralSemaphore(name, true, timeout)
-                    .join().getValue();
+            lease = new Lease(name);
         } catch (Exception ex) {
             LOG.debug("Failed to acquire the semaphore {}", name, ex);
             return false;
         }
         synchronized (leases) {
-            leases.put(name, lease);
+            leases.put(lease.name, lease);
         }
         LOG.info("Lock `{}` obtained.", name);
         return true;
@@ -133,21 +114,60 @@ public class MvLocker implements AutoCloseable {
 
     public boolean release(String name) {
         name = YdbConnector.safe(name);
-        SemaphoreLease lease;
+        Lease lease;
         synchronized (leases) {
             lease = leases.remove(name);
         }
         if (lease == null) {
             return false;
         }
+        boolean success = true;
         try {
-            lease.release().join();
-            LOG.info("Lock `{}` released.", name);
-            return true;
+            lease.semaphore.release().get(30L, TimeUnit.SECONDS);
         } catch (Exception ex) {
+            success = false;
             LOG.warn("Failed to release the lock `{}`", name, ex);
-            return false;
         }
+        try {
+            lease.session.close();
+        } catch (Exception ex) {
+            success = false;
+            LOG.warn("Failed to close the session for lock `{}`", name, ex);
+        }
+        LOG.info("Lock `{}` released.", name);
+        return success;
+    }
+
+    private CoordinationSession obtainSession() {
+        return retryCtx.supplyResult(
+                qs -> {
+                    var session = client.createSession(nodePath);
+                    return session.connect().thenApplyAsync(
+                            status -> status.isSuccess() ? Result.success(session) : Result.fail(status)
+                    );
+                }
+        ).join().getValue();
+    }
+
+    private class Lease {
+
+        final String name;
+        final CoordinationSession session;
+        final SemaphoreLease semaphore;
+
+        Lease(String name) {
+            this.name = name;
+            this.session = obtainSession();
+            try {
+                this.semaphore = this.session
+                        .acquireEphemeralSemaphore(name, true, timeout)
+                        .join().getValue();
+            } catch (Exception ex) {
+                this.session.close();
+                throw new RuntimeException("Failed to obtain lock " + name, ex);
+            }
+        }
+
     }
 
 }
